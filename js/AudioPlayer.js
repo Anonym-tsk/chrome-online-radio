3825dfb2f8dd34d6e34d5b1f38143711a87444b4
Nikolay Vasilchuk <n.vasilchuk@lancktele.com>
2013-08-02 17:09:11 +0400

    Comments

--- js/AudioPlayer.js
+++ js/AudioPlayer.js
@@ -1,4 +1,8 @@
 ﻿(function(window) {
+  /**
+   * Audio player.
+   * @constructor
+   */
   var AudioPlayer = function() {
     this._audio = document.createElement('audio');
     this._audio.preload = 'auto';
@@ -15,6 +19,11 @@
   };
 
   AudioPlayer.prototype = {
+    /**
+     * Start playing.
+     * @param url Stream (or file) url.
+     * @returns {*} AudioPlayer
+     */
     play: function(url) {
       url = url || this._audio.src;
       this._audio.src = url;
@@ -22,21 +31,38 @@
       return this;
     },
 
+    /**
+     * Stop playing.
+     * @returns {*} AudioPlayer
+     */
     stop: function() {
       this._audio.pause();
       this._audio.src = '';
       return this;
     },
 
+    /**
+     * Set player volume.
+     * @param volume Volume value from 0 to 100.
+     * @returns {*} AudioPlayer
+     */
     setVolume: function(volume) {
       this._audio.volume = volume/100;
       return this;
     },
 
+    /**
+     * Is playing now?
+     * @returns {boolean}
+     */
     isPlaying: function() {
       return !this._audio.paused && !this._audio.ended && (this._audio.readyState === 4 || this._audio.networkState === 2);
     },
 
+    /**
+     * Returns players error.
+     * @returns {Function|Error|MediaError|Function|Function|.ajax.error|On.error|Function|C.error}
+     */
     getError: function() {
       return this._audio.error;
     }
