51955e9f14e28fef18efe780a88b9f16f334377b
Nikolay Vasilchuk <n.vasilchuk@lancktele.com>
2013-08-02 17:06:32 +0400

    Comments

--- js/DataStorage.js
+++ js/DataStorage.js
@@ -1,4 +1,8 @@
 ﻿(function(window) {
+  /**
+   * Base storage class.
+   * @constructor
+   */
   var DataStorage = function() {
     this._favorites = JSON.parse(localStorage.getItem('_favorites')) || {};
     // TODO: При инициализации нужно переписывать веса, чтобы небыло бесконечного роста веса
@@ -20,10 +24,20 @@
   };
 
   DataStorage.prototype = {
+    /**
+     * Save value to localStorage.
+     * @param name
+     * @param value
+     * @private
+     */
     _save: function(name, value) {
       localStorage.setItem(name, value);
     },
 
+    /**
+     * Update stations list with core and users stations.
+     * @private
+     */
     _updateStationsList: function() {
       var stations = {};
       for (var i in this._coreStations) if (this._coreStations.hasOwnProperty(i)) {
@@ -37,12 +51,20 @@
       this._stations = stations;
     },
 
+    /**
+     * Add a station to favorites.
+     * @param name Station name.
+     */
     like: function(name) {
       var keys = Object.keys(this._favorites);
       this._favorites[name] = keys.length > 0 ? this._favorites[keys[keys.length - 1]] + 1 : 1;
       this._save('_favorites', JSON.stringify(this._favorites));
     },
 
+    /**
+     * Remove a station from favorites.
+     * @param name Station name.
+     */
     dislike: function(name) {
       if (this.isFavorite(name)) {
         delete this._favorites[name];
@@ -50,18 +72,36 @@
       this._save('_favorites', JSON.stringify(this._favorites));
     },
 
+    /**
+     * Check station in favorites.
+     * @param name Station name.
+     * @returns {*}|false
+     */
     isFavorite: function(name) {
       return this._favorites.hasOwnProperty(name) ? this._favorites[name] : false;
     },
 
+    /**
+     * Get names of favorites.
+     * @returns {*}
+     */
     getFavorites: function() {
       return this._favorites;
     },
 
+    /**
+     * Get all stations.
+     * @returns {*}
+     */
     getStations: function() {
       return this._stations;
     },
 
+    /**
+     * Get station by name.
+     * @param name Station name.
+     * @returns {*}|null
+     */
     getStationByName: function(name) {
       if (this._stations.hasOwnProperty(name)) {
         var station = this._stations[name];
@@ -71,19 +111,35 @@
       return null;
     },
 
+    /**
+     * Set the last played station.
+     * @param name Station name.
+     */
     setLast: function(name) {
       this._last = name;
       this._save('_last', this._last);
     },
 
+    /**
+     * Get the last played station name.
+     * @returns string
+     */
     getLastName: function() {
       return this._last;
     },
 
+    /**
+     * Get the last played station.
+     * @returns {*}
+     */
     getLastStation: function() {
       return this.getStationByName(this._last);
     },
 
+    /**
+     * Save users station.
+     * @param station Station object.
+     */
     addStation: function(station) {
       var keys = Object.keys(this._userStations);
       var newKey = keys.length > 0 ? parseInt(keys[keys.length - 1]) + 1 : 1;
@@ -92,6 +148,10 @@
       this._updateStationsList();
     },
 
+    /**
+     * Delete users station.
+     * @param name Station name.
+     */
     deleteStation: function(name) {
       if (this._userStations.hasOwnProperty(name)) {
         delete this._userStations[name];
