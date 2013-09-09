7a1362a80c789d79f3257f778c3713e66200c99e
Nikolay Vasilchuk <n.vasilchuk@lancktele.com>
2013-09-09 15:35:58 +0400

    Build script

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #! /bin/sh
 
 CURRENT_DIR="${PWD}"
+DIR="$(cd "$(dirname "$0")" && pwd)"
 TEMP_DIR="/tmp/chrome-online-radio"
-ZIP_NAME="chrome-online-radio.zip"
+VERSION="$(cat ""${DIR}"/manifest.json" | sed -n -e '/"version":/ s/^.*"\(.*\)".*/\1/p')"
+ZIP_NAME="chrome-online-radio-"${VERSION}".zip"
 
-git clone https://github.com/Anonym-tsk/chrome-online-radio.git ${TEMP_DIR}
-cd ${TEMP_DIR}
+git clone https://github.com/Anonym-tsk/chrome-online-radio.git "${TEMP_DIR}"
+cd "${TEMP_DIR}"
 git checkout master
 
 rm -rf .git
@@ -15,7 +17,7 @@ rm LICENSE
 rm README.md
 rm build.sh
 
-zip -r ${ZIP_NAME} *
-mv ${ZIP_NAME} ${CURRENT_DIR}
-cd ${CURRENT_DIR}
-rm -rf ${TEMP_DIR}
+zip -r "${ZIP_NAME}" *
+mv "${ZIP_NAME}" "${CURRENT_DIR}"
+cd "${CURRENT_DIR}"
+rm -rf "${TEMP_DIR}"
