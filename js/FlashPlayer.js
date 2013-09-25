055a0d6eb5cebd70aa8150d5ba6702ed8ece772b
Nikolay Vasilchuk <n.vasilchuk@lancktele.com>
2013-09-25 20:09:22 +0400

    #6 fixed Fake equalizer for Flash Player

--- js/FlashPlayer.js
+++ js/FlashPlayer.js
@@ -112,6 +112,52 @@
     },
 
     /**
+     * Render equalizer in popup window.
+     * @param canvas
+     */
+    equalizer: function(canvas) {
+      const BAR_WIDTH = 3; // Ширина полоски
+      const SPACER_WIDTH = 1; // Ширина отступа
+
+      var canvasWidth = canvas.width,
+          canvasHeight = canvas.height;
+      var numBars = Math.round(canvasWidth / (SPACER_WIDTH + BAR_WIDTH));
+
+      // Canvas context
+      var canvasContext = canvas.getContext('2d');
+      var gradient = canvasContext.createLinearGradient(0, 0, 0, canvasHeight);
+      gradient.addColorStop(1,'#0088cc');
+      gradient.addColorStop(0.5,'#00719f');
+      gradient.addColorStop(0,'#005E84');
+      canvasContext.fillStyle = gradient;
+
+      var freqByteData = [];
+      var drawFrame = function(win, time) {
+        win.requestAnimationFrame(drawFrame.bind(this, win), canvas);
+        canvasContext.clearRect(0, 0, canvasWidth, canvasHeight);
+
+        var isKeyFrame = !(Math.round(time) % 10);
+        for (var i = 0; i < numBars; ++i) {
+          if (isKeyFrame && this._isPlaying) {
+            var magnitude = Math.floor(Math.random() * canvasHeight) + 1;
+            if (!freqByteData[i] || magnitude > freqByteData[i]) {
+              freqByteData[i] = magnitude;
+            }
+          }
+          else if (freqByteData[i] > 1) {
+            freqByteData[i] -= 1;
+          }
+          else {
+            freqByteData[i] = 1;
+          }
+          canvasContext.fillRect(i * (SPACER_WIDTH + BAR_WIDTH), canvasHeight, BAR_WIDTH, -freqByteData[i]);
+        }
+      };
+
+      drawFrame.call(this, canvas.ownerDocument.defaultView, 0);
+    },
+
+    /**
      * Send message to fallback page.
      * @param {String} action
      * @param {*} [data]
