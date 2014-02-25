287ce1aa6aa922887626884ca17e1ab418914615
Nikolay Vasilchuk <Anonym.tsk@gmail.com>
2014-02-25 16:50:08 +0400

    Refactoring

--- js/background.js
+++ js/background.js
@@ -5,7 +5,7 @@
    * Main application class.
    * @constructor
    */
-  var Radio = function() {
+  function Radio() {
     var EMPTY_PORT = {postMessage: function(data) {}};
 
     this.Storage = new DataStorage();
@@ -29,7 +29,7 @@
         return;
       }
 
-      var portListener = function(message) {
+      var portListener = (function(message) {
         console.log('Message to bg port', message);
         var volume, volStep = 5, stations, name;
 
@@ -116,7 +116,7 @@
             this.openOptions(message.data);
             break;
         }
-      }.bind(this);
+      }).bind(this);
 
       if (port.name == 'hotkey') {
         port.postMessage(this.Storage.getHotkeys());
@@ -147,188 +147,191 @@
     this.updateContextMenu();
     this.initEvents();
     this.setStatus();
-  };
+  }
 
-  Radio.prototype = {
-    /**
-     * Init player events.
-     */
-    initEvents: function() {
-      this.Player.bind('play', function() {
-        this.setStatus('buffering');
-        this.sendMessage(this.status);
-      }, this);
-      this.Player.bind('playing', function() {
-        this._attempts = 0;
-        this.setStatus('playing');
-        this.sendMessage(this.status);
-      }, this);
-      this.Player.bind('abort', function() {
-        this.setStatus('stopped');
-        this.sendMessage(this.status);
-      }, this);
-      this.Player.bind('error', function() {
-        if (this.status !== 'stopped') {
-          if (this._attempts++ < 4) {
-            this.Player.play();
-          }
-          else {
-            this._attempts = 0;
-            this.setStatus('error');
-            this.sendMessage(this.status);
-          }
-        }
-      }, this);
-    },
-
-    /**
-     * Open options page.
-     * @param {string} page
-     */
-    openOptions: function(page) {
-      var optionsUrl = chrome.extension.getURL('options.html');
-      var fullUrl = (typeof page == 'string') ? optionsUrl + '#' + page : optionsUrl;
-      chrome.tabs.query({url: optionsUrl}, function(tabs) {
-        if (tabs.length) {
-          chrome.tabs.update(tabs[0].id, {active: true, url: fullUrl});
-          chrome.tabs.reload(tabs[0].id);
+  /**
+   * Init player events.
+   */
+  Radio.prototype.initEvents = function() {
+    this.Player.bind('play', function() {
+      this.setStatus('buffering');
+      this.sendMessage(this.status);
+    }, this);
+    this.Player.bind('playing', function() {
+      this._attempts = 0;
+      this.setStatus('playing');
+      this.sendMessage(this.status);
+    }, this);
+    this.Player.bind('abort', function() {
+      this.setStatus('stopped');
+      this.sendMessage(this.status);
+    }, this);
+    this.Player.bind('error', function() {
+      if (this.status !== 'stopped') {
+        if (this._attempts++ < 4) {
+          this.Player.play();
         }
         else {
-          chrome.tabs.create({url: fullUrl});
+          this._attempts = 0;
+          this.setStatus('error');
+          this.sendMessage(this.status);
         }
-      });
-    },
-
-    /**
-     * Set browser icon text and color.
-     * @param {string=} text
-     * @param {string|Array=} color
-     */
-    setIconText: function(text, color) {
-      text = text || '';
-      color = color || [255, 79, 87, 255];
-      chrome.browserAction.setBadgeBackgroundColor({color: color});
-      chrome.browserAction.setBadgeText({text: text});
-    },
-
-    setStatus: function(status) {
-      this.status = status || 'stopped';
-
-      switch (status) {
-        case 'buffering':
-          this.setIconText('…', [255, 144, 0, 255]);
-          chrome.browserAction.setIcon({path: {'19': 'icons/19o.png', '38': 'icons/38o.png'}});
-          chrome.browserAction.setTitle({title: this.Storage.getLastStation().title + ' - ' + chrome.i18n.getMessage('loading')});
-          break;
-        case 'playing':
-          this.setIconText('►', [0, 180, 0, 255]);
-          chrome.browserAction.setIcon({path: {'19': 'icons/19g.png', '38': 'icons/38g.png'}});
-          chrome.browserAction.setTitle({title: this.Storage.getLastStation().title});
-          break;
-        case 'stopped':
-          this.setIconText();
-          chrome.browserAction.setIcon({path: {'19': 'icons/19.png', '38': 'icons/38.png'}});
-          chrome.browserAction.setTitle({title: this.Storage.getLastStation().title + ' - ' + chrome.i18n.getMessage('stopped')});
-          break;
-        case 'error':
-          this.setIconText();
-          chrome.browserAction.setIcon({path: {'19': 'icons/19r.png', '38': 'icons/38r.png'}});
-          chrome.browserAction.setTitle({title: this.Storage.getLastStation().title + ' - ' + chrome.i18n.getMessage('error')});
-          break;
-        default:
-          this.setIconText();
-          chrome.browserAction.setIcon({path: {'19': 'icons/19.png', '38': 'icons/38.png'}});
-          chrome.browserAction.setTitle({title: chrome.i18n.getMessage('name')});
       }
-    },
+    }, this);
+  };
 
-    /**
-     * Send message to opened port (popup by default).
-     * @param {string} action
-     * @param {string} data
-     */
-    sendMessage: function(action, data) {
-      data = data || {};
-      this._port.postMessage({action: action, data: data});
-    },
-
-    /**
-     * Show desktop notification.
-     * @param {string} title
-     * @param {string} icon
-     * @param {number} timeout
-     */
-    showNotification: function(title, icon, timeout) {
-      if (!window.webkitNotifications) {
-        return; // Opera, you will die!
+  /**
+   * Open options page.
+   * @param {string} page
+   */
+  Radio.prototype.openOptions = function(page) {
+    var optionsUrl = chrome.extension.getURL('options.html');
+    var fullUrl = (typeof page == 'string') ? optionsUrl + '#' + page : optionsUrl;
+    chrome.tabs.query({url: optionsUrl}, function(tabs) {
+      if (tabs.length) {
+        chrome.tabs.update(tabs[0].id, {active: true, url: fullUrl});
+        chrome.tabs.reload(tabs[0].id);
       }
-      timeout = timeout || 5000;
-      if (this._notification.timeout) {
-        this._notification.close();
+      else {
+        chrome.tabs.create({url: fullUrl});
       }
-      this._notification.notify = new Notification(title, {icon: icon ? icon : 'icons/38.png'});
-      this._notification.notify.onclick = this._notification.close;
-      this._notification.notify.onshow = function() {
-        this._notification.timeout = setTimeout(this._notification.close, timeout);
-      }.bind(this);
-    },
+    });
+  };
 
-    /**
-     * Save found audio stream to history.
-     * @param {Object} response
-     */
-    addAudioHistory: function(response) {
-      chrome.tabs.get(response.tabId, function(tab) {
-        this._foundStreams.push({
-          title: tab.title,
-          stream: response.url,
-          favicon: tab.favIconUrl,
-          tabId: tab.id,
-          url: tab.url
-        });
-        this._foundStreams = this._foundStreams.slice(-15, this._foundStreams.length);
-        this.updateContextMenu();
-      }.bind(this));
-    },
+  /**
+   * Set browser icon text and color.
+   * @param {string=} text
+   * @param {string|Array=} color
+   */
+  Radio.prototype.setIconText = function(text, color) {
+    text = text || '';
+    color = color || [255, 79, 87, 255];
+    chrome.browserAction.setBadgeBackgroundColor({color: color});
+    chrome.browserAction.setBadgeText({text: text});
+  };
 
-    /**
-     * Update context menu by audio history.
-     */
-    updateContextMenu: function() {
-      chrome.contextMenus.removeAll(function() {
-        var contexts = ['page', 'frame', 'selection'];
-        chrome.contextMenus.create({
-          title: chrome.i18n.getMessage('add'),
-          contexts: contexts,
-          enabled: false
-        });
-        chrome.contextMenus.create({
-          type: 'separator',
-          contexts: contexts
-        });
+  /**
+   * Set radio playing status.
+   * @param {string=} status
+   */
+  Radio.prototype.setStatus = function(status) {
+    this.status = status || 'stopped';
 
-        var i = this._foundStreams.length;
-        if (!i) {
-          chrome.contextMenus.create({
-            title: chrome.i18n.getMessage('please_enable_radio'),
-            contexts: contexts,
-            enabled: false,
-            id: 'online_radio'
-          });
-        }
-        else while (i--) {
-          chrome.contextMenus.create({
-            title: this._foundStreams[i].title,
-            contexts: contexts,
-            onclick: function(data) {
-              return function(info, tab) {
-                this.openOptions('add#' + data.title + '#' + data.stream + '#' + data.url);
-              }.bind(this);
-            }.call(this, this._foundStreams[i])
-          });
-        }
-      }.bind(this));
+    switch (status) {
+      case 'buffering':
+        this.setIconText('…', [255, 144, 0, 255]);
+        chrome.browserAction.setIcon({path: {'19': 'icons/19o.png', '38': 'icons/38o.png'}});
+        chrome.browserAction.setTitle({title: this.Storage.getLastStation().title + ' - ' + chrome.i18n.getMessage('loading')});
+        break;
+      case 'playing':
+        this.setIconText('►', [0, 180, 0, 255]);
+        chrome.browserAction.setIcon({path: {'19': 'icons/19g.png', '38': 'icons/38g.png'}});
+        chrome.browserAction.setTitle({title: this.Storage.getLastStation().title});
+        break;
+      case 'stopped':
+        this.setIconText();
+        chrome.browserAction.setIcon({path: {'19': 'icons/19.png', '38': 'icons/38.png'}});
+        chrome.browserAction.setTitle({title: this.Storage.getLastStation().title + ' - ' + chrome.i18n.getMessage('stopped')});
+        break;
+      case 'error':
+        this.setIconText();
+        chrome.browserAction.setIcon({path: {'19': 'icons/19r.png', '38': 'icons/38r.png'}});
+        chrome.browserAction.setTitle({title: this.Storage.getLastStation().title + ' - ' + chrome.i18n.getMessage('error')});
+        break;
+      default:
+        this.setIconText();
+        chrome.browserAction.setIcon({path: {'19': 'icons/19.png', '38': 'icons/38.png'}});
+        chrome.browserAction.setTitle({title: chrome.i18n.getMessage('name')});
     }
   };
+
+  /**
+   * Send message to opened port (popup by default).
+   * @param {string} action
+   * @param {string=} data
+   */
+  Radio.prototype.sendMessage = function(action, data) {
+    data = data || {};
+    this._port.postMessage({action: action, data: data});
+  };
+
+  /**
+   * Show desktop notification.
+   * @param {string} title
+   * @param {string} icon
+   * @param {number=} timeout
+   */
+  Radio.prototype.showNotification = function(title, icon, timeout) {
+    if (!window.webkitNotifications) {
+      return; // Opera, you will die!
+    }
+    timeout = timeout || 5000;
+    if (this._notification.timeout) {
+      this._notification.close();
+    }
+    this._notification.notify = new Notification(title, {icon: icon ? icon : 'icons/38.png'});
+    this._notification.notify.onclick = this._notification.close;
+    this._notification.notify.onshow = (function() {
+      this._notification.timeout = setTimeout(this._notification.close, timeout);
+    }).bind(this);
+  };
+
+  /**
+   * Save found audio stream to history.
+   * @param {Object} response
+   */
+  Radio.prototype.addAudioHistory = function(response) {
+    chrome.tabs.get(response.tabId, function(tab) {
+      this._foundStreams.push({
+        title: tab.title,
+        stream: response.url,
+        favicon: tab.favIconUrl,
+        tabId: tab.id,
+        url: tab.url
+      });
+      this._foundStreams = this._foundStreams.slice(-15, this._foundStreams.length);
+      this.updateContextMenu();
+    }.bind(this));
+  };
+
+  /**
+   * Update context menu by audio history.
+   */
+  Radio.prototype.updateContextMenu = function() {
+    chrome.contextMenus.removeAll(function() {
+      var contexts = ['page', 'frame', 'selection'];
+      chrome.contextMenus.create({
+        title: chrome.i18n.getMessage('add'),
+        contexts: contexts,
+        enabled: false
+      });
+      chrome.contextMenus.create({
+        type: 'separator',
+        contexts: contexts
+      });
+
+      var i = this._foundStreams.length;
+      if (!i) {
+        chrome.contextMenus.create({
+          title: chrome.i18n.getMessage('please_enable_radio'),
+          contexts: contexts,
+          enabled: false,
+          id: 'online_radio'
+        });
+      }
+      else while (i--) {
+        chrome.contextMenus.create({
+          title: this._foundStreams[i].title,
+          contexts: contexts,
+          onclick: function(data) {
+            return function() {
+              this.openOptions('add#' + data.title + '#' + data.stream + '#' + data.url);
+            }.bind(this);
+          }.call(this, this._foundStreams[i])
+        });
+      }
+    }.bind(this));
+  };
+
   window.Radio = new Radio();
 })(window);
