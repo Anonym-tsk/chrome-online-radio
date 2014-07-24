55c4133a700e0140b0ee9df8383a170f7a450712
Nikolay Vasilchuk <nikolay.vasilchuk@corp.mail.ru>
2014-07-24 13:07:05 +0400

    Refactoring

--- js/ext/proxy.js
+++ js/ext/proxy.js
@@ -1,5 +1,4 @@
 /**
- * Proxy interface.
  * Proxy messages from web page to background.
  * Proxy messages from background to web page.
  */
@@ -7,34 +6,11 @@
   if (window === window.top) {
     return;
   }
+
   /**
    * Extension background port.
    */
   var port = chrome.runtime.connect({name: 'proxy'});
-
-  /**
-   * Send message to background extension.
-   * @param action
-   * @param data
-   */
-  var sendToBackground = function(action, data) {
-    data = data || {};
-    port.postMessage({action: action, data: data});
-  };
-
-  /**
-   * Send message to fallback web page.
-   * @param action
-   * @param data
-   */
-  var sendToFallback = function(action, data) {
-    var message = {
-      type: 'fallback_to',
-      action: action,
-      data: data || {}
-    };
-    window.postMessage(message, '*');
-  };
 
   /**
    * Proxy messages from background to web page.
@@ -54,4 +30,27 @@
       sendToBackground(event.data.action, event.data.data);
     }
   }, false);
+
+  /**
+   * Send message to background extension.
+   * @param {string} action
+   * @param {string=} data
+   */
+  function sendToBackground(action, data) {
+    port.postMessage({action: action, data: typeof data != 'undefined' ? data : null});
+  }
+
+  /**
+   * Send message to fallback web page.
+   * @param {string} action
+   * @param {string} data
+   */
+  function sendToFallback(action, data) {
+    var message = {
+      type: 'fallback_to',
+      action: action,
+      data: typeof data != 'undefined' ? data : null
+    };
+    window.postMessage(message, '*');
+  }
 })(window);
