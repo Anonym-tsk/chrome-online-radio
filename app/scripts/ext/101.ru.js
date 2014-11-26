e609c13a2f511ec1a403f1ea424987116f8d6b1e
Nikolay Vasilchuk <Anonym.tsk@gmail.com>
2014-11-26 15:21:35 +0300

    101.ru фикс пользовательских станций

--- app/scripts/ext/101.ru.js
+++ app/scripts/ext/101.ru.js
@@ -19,12 +19,10 @@
   }
 
   function getPlaylistUrl() {
-    var playerId = playButton && playButton.getAttribute('playerid'),
-      playerObject = playerId && document.getElementById(playerId),
-      flashVarsParam = playerObject && playerObject.querySelector('param[name="flashvars"]'),
-      flashvars = flashVarsParam && decodeURIComponent(flashVarsParam.getAttribute('value')),
-      playlist = flashvars && /pl\=([^&]+)\&/.exec(flashvars);
-    return playlist && playlist[1] && playlist[1].split('|').join('&');
+    var script = document.querySelector('#player-site .channel-base script'),
+        scriptContent = script && script.innerText,
+        playlist = scriptContent && scriptContent.match(/[\'\"]pl[\"\']\s?\:\s?[\'\"]([^\'^\"]+)[\"\']/);
+    return playlist && playlist[1] && decodeURIComponent(playlist[1].split('|').join('&'));
   }
 
   function injectStyles() {
@@ -47,11 +45,7 @@
     document.getElementsByTagName('head')[0].appendChild(style);
   }
 
-  function loadPlaylist(successCallback) {
-    var playlistUrl = getPlaylistUrl();
-    if (!playlistUrl) {
-      return [];
-    }
+  function loadPlaylist(playlistUrl, successCallback) {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
       if (xhr.readyState === 4 && xhr.status === 200) {
@@ -72,11 +66,16 @@
     chrome.runtime.sendMessage({name: 'background', action: action, data: typeof data !== 'undefined' ? data : null});
   }
 
+  var playlistUrl = getPlaylistUrl();
+  if (!playlistUrl) {
+    return;
+  }
+
   var button = document.createElement('span');
   button.className = 'online-radio-add-button';
   button.title = chrome.i18n.getMessage('add');
   button.onclick = function() {
-    loadPlaylist(function(streams) {
+    loadPlaylist(playlistUrl, function(streams) {
       stop();
 
       var link = document.querySelector('#channel_infoblock a:not([target])'),
